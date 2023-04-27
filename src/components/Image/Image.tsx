6634da20d2b06bf151b7e1c6f85b2d62ff4d6d53
Aleksandr Yermolaev <sanyagt50@gmail.com>
2023-04-27 15:03:56 +0300

    fix: remove eslint warnings

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,24 +1,12 @@
-import { useState } from 'react';
-import ImageSkeleton from 'components/Skeletons/ImageSkeleton';
-
 type ImageProps = {
   src: string;
   name: string;
 };
 
 const Image: React.FC<ImageProps> = ({ src, name }) => {
-  const [imageIsLoad, setImageIsLoad] = useState<boolean>(false);
-
   return (
     <>
-      {/* {imageIsLoad ? null : <ImageSkeleton />} */}
-      <img
-        src={src}
-        alt={`Avatar of ${name}`}
-        height={300}
-        width={300}
-        onLoad={() => setImageIsLoad(true)}
-      />
+      <img src={src} alt={`Avatar of ${name}`} height={300} width={300} />
     </>
   );
 };
